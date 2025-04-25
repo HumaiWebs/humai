041ad5517c8f26a1ee43a8096989494a8242078f
Mudassirpk <mudassir.pk.mp@gmail.com>
2025-04-25 17:25:39 +0500

    feat: implement dropdown menu in Header component and refactor navigation structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
 import About from "./components/about";
 import Approach from "./components/approach";
-import ClientSaying from "./components/clients-saying";
+import Footer from "./components/footer";
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Services from "./components/services";
-import TechnologiesSection from "./components/technologies";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <section className="relative text-white lg:flex-row  overflow-hidden">
+      <section className="relative -z-1 text-white lg:flex-row  overflow-hidden">
         <Hero />
       </section>
       <Services />
       <About />
       <Approach />
       <Footer />
-      \\\\
     </>
   );
 }
